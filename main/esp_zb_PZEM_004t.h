6085469dbb1da10a7f23de9e7f98b64986aa6a28
Edgar Rivera <58781909+eddr10@users.noreply.github.com>
2024-07-26 13:12:24 -0500

    Add files via upload

---
+++ main/esp_zb_PZEM_004t.h
@@ -0,0 +1,37 @@
+#include "esp_zigbee_core.h"
+
+/* Zigbee configuration */
+#define INSTALLCODE_POLICY_ENABLE       false   /* enable the install code policy for security */
+#define ED_AGING_TIMEOUT                ESP_ZB_ED_AGING_TIMEOUT_64MIN
+#define ED_KEEP_ALIVE                   3000    /* 3000 millisecond */
+#define HA_SENSOR_ENDPOINT  10                                    /* esp light switch device endpoint */
+#define ESP_ZB_PRIMARY_CHANNEL_MASK     ESP_ZB_TRANSCEIVER_ALL_CHANNELS_MASK    /* Zigbee primary channel mask use in the example */
+
+#define ESP_TEMP_SENSOR_UPDATE_INTERVAL (1)     /* Local sensor update interval (second) */
+#define ESP_TEMP_SENSOR_MIN_VALUE       (-10)   /* Local sensor min measured value (degree Celsius) */
+#define ESP_TEMP_SENSOR_MAX_VALUE       (80)    /* Local sensor max measured value (degree Celsius) */
+
+
+#define MANUFACTURER_NAME               "\x10""SDPC_2022_2"
+#define MODEL_IDENTIFIER                "\x20""Zigbee_ESP32C6"
+#define MODEL_NAME                      "\r""ESP32C6_EndPoint"
+
+#define ESP_ZB_ZED_CONFIG()                                         \
+    {                                                               \
+        .esp_zb_role = ESP_ZB_DEVICE_TYPE_ED,                       \
+        .install_code_policy = INSTALLCODE_POLICY_ENABLE,           \
+        .nwk_cfg.zed_cfg = {                                        \
+            .ed_timeout = ED_AGING_TIMEOUT,                         \
+            .keep_alive = ED_KEEP_ALIVE,                            \
+        },                                                          \
+    }
+
+#define ESP_ZB_DEFAULT_RADIO_CONFIG()                           \
+    {                                                           \
+        .radio_mode = RADIO_MODE_NATIVE,                     \
+    }
+
+#define ESP_ZB_DEFAULT_HOST_CONFIG()                            \
+    {                                                           \
+        .host_connection_mode = HOST_CONNECTION_MODE_NONE,   \
+    }
